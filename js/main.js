4e0cee07b2400c3b782bcae5a4c6275e3de66427
nidgo <74098023+nidgo@users.noreply.github.com>
2020-11-09 18:20:02 +0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -53,16 +53,10 @@ downloadButton.addEventListener('click', () => {
   
   
   var oData = new FormData(form);
-
   oData.append("testvid", blob, 'test.webm');
-
   var oReq = new XMLHttpRequest();
   oReq.open("POST", "stash.php", true);
-  
-
   oReq.send(oData);
-  ev.preventDefault();
-}, false);
   
   
   setTimeout(() => {
