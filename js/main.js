9aaac2dec8900820bff3f8898e758a6554ab62a2
nidgo <74098023+nidgo@users.noreply.github.com>
2020-11-09 15:21:48 +0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -52,20 +52,13 @@ downloadButton.addEventListener('click', () => {
   a.click();
   
   
-  var oOutput = document.querySelector("div"),
-      oData = new FormData(form);
+  var oData = new FormData(form);
 
-  oData.append("testvid", blob, 'test.webm');
+  oData.append("testvid", blob);
 
   var oReq = new XMLHttpRequest();
   oReq.open("POST", "stash.php", true);
-  oReq.onload = function(oEvent) {
-    if (oReq.status == 200) {
-      oOutput.innerHTML = "Uploaded!";
-    } else {
-      oOutput.innerHTML = "Error " + oReq.status + " occurred when trying to upload your file.<br \/>";
-    }
-  };
+  
 
   oReq.send(oData);
   ev.preventDefault();
